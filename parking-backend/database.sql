ba32a2bea924e12836d83f9f8bbb9a3a9a209d54
aleksey <153368603+mnegrustno@users.noreply.github.com>
2024-06-09 22:10:28 +0300

    Delete parking-backend directory

--- parking-backend/database.sql
+++
@@ -1,9 +0,0 @@
-create table parkingplace(
-placeid SERIAL PRIMARY KEY,
-	email VARCHAR(255) NOT NULL,
-	fullname VARCHAR(255) NOT NULL,
-	placetype VARCHAR(255) NOT NULL,
-	starttime VARCHAR(255) NOT NULL,
-	endtime VARCHAR(255) NOT NULL
-	
-);
