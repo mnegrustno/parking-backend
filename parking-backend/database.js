5cebd54d1bbb28ff7db3606a163bae51d0fa6d9a
aleksey <153368603+mnegrustno@users.noreply.github.com>
2024-06-09 21:44:55 +0300

    Add files via upload

---
+++ parking-backend/database.js
@@ -0,0 +1,13 @@
+//Подключение к бд
+const Pool = require('pg').Pool
+const pool = new Pool(
+    {
+        user:"username",//имя для входа в бд
+        password:"password",//пароль
+        host:"hostname",//имя хоста
+        port:5432,//порт
+        database:"databsename"//имя базы данных
+    }
+)
+
+module.exports = pool
---
+++ parking-backend/routes/routes.js
@@ -0,0 +1,10 @@
+const Router = require('express')
+
+const router = new Router()
+
+
+const parkingController = require('../controller/parking.controller')
+
+router.post('/createplace',parkingController.createPlace)
+
+module.exports = router;
